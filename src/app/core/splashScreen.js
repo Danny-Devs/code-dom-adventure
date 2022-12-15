d29f2113b55d710891ebc023077a7056c4af020c
Daniel Ahn <et3rnal.d@gmail.com>
2022-12-15 13:04:07 -0800

    init

---
+++ src/app/core/splashScreen.js
@@ -0,0 +1,10 @@
+class SplashScreen {
+  render () {
+    const div = document.createElement('div')
+    div.style.backgroundImage = 'url(../assets/splash-screen.jpg)'
+    document.body.appendChild(div)
+  }
+  destory () {
+
+  }
+}
